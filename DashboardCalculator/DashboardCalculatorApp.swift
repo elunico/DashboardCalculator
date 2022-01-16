af2c963dfaf8f74c12670716140a020fb5736b79
Tom <elunico@users.noreply.github.com>
2022-01-16 12:53:52 -0500

    Initial Commit

---
+++ DashboardCalculator/DashboardCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardCalculatorApp.swift
+//  DashboardCalculator
+//
+//  Created by Thomas Povinelli on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct DashboardCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
